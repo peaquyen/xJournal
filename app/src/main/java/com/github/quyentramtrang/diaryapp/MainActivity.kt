47c649ce0818a9f8de2db4bdfb5da0e7cf009dba
Bao Tram <baotram1701.ct@gmail.com>
2024-03-29 21:55:46 +0700

    begin

---
+++ app/src/main/java/com/github/quyentramtrang/diaryapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.quyentramtrang.diaryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.github.quyentramtrang.diaryapp.ui.theme.DiaryAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiaryAppTheme {
+            }
+        }
+    }
+}
